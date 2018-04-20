022db9086b9769e6c81c1ab12a88f172cac93ad3
huyu <hello_linux@163.com>
2018-04-20 14:33:50 +0800

    更新home

--- app/base/index.js
+++ app/base/index.js
@@ -3,11 +3,13 @@
  */
 
 import React, { Component } from 'react'
-import { Button, Modal, message } from 'antd'
+import { Layout, Menu, Breadcrumb, Icon } from 'antd'
 import {
     Link,
 } from 'react-router-dom'
 import { Connect } from '@components/store'
+const { SubMenu } = Menu
+const { Header, Content, Sider } = Layout
 
 @Connect((store) => ({
     config: store.config,
@@ -23,22 +25,45 @@ export default class home extends Component {
     render() {
         return (
             <div className="main-wrap">
-                <div className="main-header">
-                    header
-                    </div>
-                <div className="main-body">
-                    <div className="main-body-left">
-                        <ul>
-                            <li><Link to="/nav1">to nav1</Link></li>
-                            <li><Link to="/nav2">to nav2</Link></li>
-                            <li><Link to="/nav2">{this.props.config && this.props.config.name}</Link></li>
-                        </ul>
-                    </div>
-                    <div className="main-body-content">
-                        {this.props.children || '无children'}
-                    </div>
-                </div>
+                <Layout>
+                    <Header className="header">
+                        <div className="logo"><span>后台管理系统</span></div>
+                        <Menu
+                            theme="dark"
+                            mode="horizontal"
+                            defaultSelectedKeys={['2']}
+                            style={{ lineHeight: '64px' }}
+                        >
+                            <Menu.Item key="1"><Link to="/nav1">nav 1</Link></Menu.Item>
+                            <Menu.Item key="2"><Link to="/nav2">nav 2</Link></Menu.Item>
+                        </Menu>
+                    </Header>
+                    <Layout>
+                        <Sider width={200} style={{ background: '#fff' }}>
+                            <Menu
+                                mode="inline"
+                                theme="dark"
+                                defaultSelectedKeys={['1']}
+                                defaultOpenKeys={['sub1']}
+                                style={{ height: '100%', borderRight: 0 }}
+                            >
+                                <SubMenu key="sub1" title={<span><Icon type="user" />subnav 1</span>}>
+                                    <Menu.Item key="1">option1</Menu.Item>
+                                    <Menu.Item key="2">option2</Menu.Item>
+                                    <Menu.Item key="3">option3</Menu.Item>
+                                    <Menu.Item key="4">option4</Menu.Item>
+                                </SubMenu>
+                            </Menu>
+                        </Sider>
+                        <Layout style={{ padding: '24px' }}>
+                            <Content style={{ background: '#fff', padding: 24, margin: 0, minHeight: 280 }}>
+                                {this.props.children || '无children'}
+                            </Content>
+                        </Layout>
+                    </Layout>
+                </Layout>
             </div>
+
         )
     }
 }
