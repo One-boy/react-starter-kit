c6dcd930c6e985ab68479d9e5572ff2f77d1d47b
huyu <hello_linux@163.com>
2018-04-25 11:49:53 +0800

    增加对字体文件的处理

--- scripts/webpack.base.config.js
+++ scripts/webpack.base.config.js
@@ -2,83 +2,94 @@
 /**
  * webpack基础配置
  */
+/* eslint no-undef:0 */
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 function resolve(p) {
-    return path.join(__dirname, p)
+  return path.join(__dirname, p)
 }
 
 const webpackConfigBase = {
-    entry: {
-        client: resolve('../app/client.js')
+  entry: {
+    client: resolve('../app/client.js')
+  },
+  output: {
+    path: resolve('../dist'),
+    filename: '[name].[hash:4].js',
+    chunkFilename: 'chunks/[name].[hash:4.js]',
+  },
+  resolve: {
+    extensions: ['.js', '.json'],
+    // 文件引用路径替换，加@区别于node_modules库
+    alias: {
+      '@style': resolve('../app/style'),
+      '@api': resolve('../app/api'),
+      '@resource': resolve('../app/resource'),
+      '@components': resolve('../app/components'),
+      '@config': resolve('../app/config'),
+      '@pages': resolve('../app/pages'),
+      '@utils': resolve('../app/utils'),
     },
-    output: {
-        path: resolve('../dist'),
-        filename: '[name].[hash:4].js',
-        chunkFilename: 'chunks/[name].[hash:4.js]',
-    },
-    resolve: {
-        extensions: ['.js', '.json'],
-        // 文件引用路径替换，加@区别于node_modules库
-        alias: {
-            '@style': resolve('../app/style'),
-            '@api': resolve('../app/api'),
-            '@resource': resolve('../app/resource'),
-            '@components': resolve('../app/components'),
-            '@config': resolve('../app/config'),
-            '@pages': resolve('../app/pages'),
-            '@utils': resolve('../app/utils'),
-        },
-    },
-    module: {
-        rules: [
-            {
-                exclude: /node_modules/,
-                test: /\.(js|jsx)$/,
-                use: 'babel-loader'
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                ],
-            },
-            {
-                test: /\.less$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    { loader: "css-loader", options: { sourceMap: true } },
-                    {
-                        loader: "less-loader",
-                        options: {
-                            sourceMap: true,
-                            javascriptEnabled: true,
-                            paths: [
-                                path.resolve(__dirname, '../node_modules'),
-                                path.resolve(__dirname, '../app/style'),
-                            ]
-                        }
-                    }
-                ],
-            },
+  },
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
         ],
-    },
-    plugins: [
-        // 打包后的资源引用到html文件内
-        new HtmlWebpackPlugin({
-            template: resolve('../app/index.html'),
-        }),
-        // 提取css
-        new MiniCssExtractPlugin({
-            // Options similar to the same options in webpackOptions.output
-            // both options are optional
-            filename: "style.[hash:4].css",
-            chunkFilename: "[id].css"
-        })
+      },
+      {
+        test: /\.less$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: 'css-loader', options: { sourceMap: true } },
+          {
+            loader: 'less-loader',
+            options: {
+              sourceMap: true,
+              javascriptEnabled: true,
+              paths: [
+                resolve('../node_modules'),
+                resolve('../app/style'),
+              ]
+            }
+          }
+        ],
+      },
+      {
+        test: /\.(woff|eot|ttf|svg|gif)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 8192,
+            name: 'resource/iconfont/[hash:4].[ext]',
+          },
+        }],
+      },
     ],
+  },
+  plugins: [
+    // 打包后的资源引用到html文件内
+    new HtmlWebpackPlugin({
+      template: resolve('../app/index.html'),
+    }),
+    // 提取css
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: 'style.[hash:4].css',
+      chunkFilename: '[id].css'
+    })
+  ],
 }
 
 module.exports = webpackConfigBase
--- scripts/webpack.dev.config.js
+++ scripts/webpack.dev.config.js
@@ -1,33 +1,33 @@
 /**
  * 开发环境配置
  */
+/* eslint no-undef:0 */
 
-const webpack = require('webpack')
 const webpackConfigBase = require('./webpack.base.config')
 const merge = require('webpack-merge')
 const path = require('path')
 const OpenBrowserPlugin = require('open-browser-webpack-plugin')
 
 function resolve(p) {
-    return path.join(__dirname, p)
+  return path.join(__dirname, p)
 }
 const PORT = 8888
 
 const webpackConfigDev = {
-    mode: 'development',
-    plugins: [
-        new OpenBrowserPlugin({
-            url: `http://localhost:${PORT}/#/login`
-        }),
-    ],
-    devtool: 'source-map',
-    devServer: {
-        contentBase: resolve('../app'),
-        historyApiFallback: false,
-        hot: false,
-        host: '0.0.0.0',
-        port: PORT,
-    },
+  mode: 'development',
+  plugins: [
+    new OpenBrowserPlugin({
+      url: `http://localhost:${PORT}/#/login`
+    }),
+  ],
+  devtool: 'source-map',
+  devServer: {
+    contentBase: resolve('../app'),
+    historyApiFallback: false,
+    hot: false,
+    host: '0.0.0.0',
+    port: PORT,
+  },
 }
 
 module.exports = merge(webpackConfigBase, webpackConfigDev)
