9961d603899bfded361b056cb74102af353b9d04
huyu <hello_linux@163.com>
2018-10-13 21:19:25 +0800

    更新样式引用方式

--- app/client.js
+++ app/client.js
@@ -1,25 +1,15 @@
-// import 'babel-polyfill'
-// import 'core-js'
-// react16依赖requestAnimationFrame
-import 'raf/polyfill'
-// import 'core-js/es6/map'
-// import 'core-js/es6/set'
-// import 'core-js/modules/es6.symbol'
-// require('blob-polyfill')
-// require('formdata-polyfill')
 
+import 'raf/polyfill'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Routes from './routes'
 import { LocaleProvider } from 'antd'
 import zh_CN from 'antd/lib/locale-provider/zh_CN'
 import {
   HashRouter as Router,
 } from 'react-router-dom'
 import { Provider as StoreProvider } from './components/store'
-
-
-import './style/index.js'
+import './style/index.less'
+import Routes from './routes'
 
 // 全局store初始值
 const store = {}
