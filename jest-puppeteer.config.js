a82615e30bdaad83caf49b5e680a6d62fa69fdce
huyu <hello_linux@163.com>
2020-05-03 17:12:17 +0800

    puppeteer铺满全屏

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -2,7 +2,7 @@
  * @Author: huyu 
  * @Date: 2020-04-30 10:51:35 
  * @Last Modified by: huyu
- * @Last Modified time: 2020-05-03 10:51:07
+ * @Last Modified time: 2020-05-03 17:12:02
  */
 
 // jest-puppeteer配置文件
@@ -14,17 +14,14 @@ module.exports = {
   launch: {
     executablePath: './.local-chromium/win64-737027/chrome-win/chrome.exe', // chrome执行程序路径
     headless: false, // 无头模式
-    defaultViewport: { // 页面窗口高宽
-      width: 1200,
-      height: 768,
-      deviceScaleFactor: 1
-    },
+    defaultViewport: null,
     args: [
       '--disable-gpu',
       '--disable-dev-shm-usage',
       '--no-first-run',
       '--no-zygote',
-      '--no-sandbox'
+      '--no-sandbox',
+      '--start-maximized'
     ],
   },
 }
